aeb913a8131cf01a72fccdf183e7ae0b06a0698c
xsenechal <xsenechal@gmail.com>
2015-07-08 22:43:54 +0200

    add criteriaToSql

--- static/app.js
+++ static/app.js
@@ -2,15 +2,21 @@ angular.module('sql-browser', ['ui.bootstrap', 'cfp.hotkeys'])
     .controller('MainCtrl', MainCtrl);
 
 function MainCtrl($scope, $http, $log, hotkeys, $document, $modal, orderByFilter) {
-    $scope.requestMode = 'Manual';
+    $scope.requestMode = 'Form';
     $scope.con = {
         url: 'jdbc:mysql://localhost:3306/wp',
         user: "root",
-        password: ""
+        pwd: ""
     };
-    $scope.request = 'select * from information_schema.tables;'//"select * from users;"
+    $scope.request = 'select * from information_schema.tables;';//"select * from users;"
     $scope.headers = [];
     $scope.exeRequest = function(){
+        switch ($scope.requestMode) {
+            case'Form':
+                $scope.criteriaToSql($scope.table, $scope.metadata.columns, $scope.criterias);
+                break;
+        }
+
        $http.post('request', {con:$scope.con, request: $scope.request}).success(function(rows){
            $scope.headers = _.keys(rows[0]);
            $scope.rows = rows;
@@ -23,23 +29,47 @@ function MainCtrl($scope, $http, $log, hotkeys, $document, $modal, orderByFilter
             $scope.exeRequest();
         }
     });
+    $scope.criteriaToSql = function(table, columns, criterias){
+        var isFirst = true, sql = 'SELECT * FROM ' + table;
+        if(_.size(criterias) > 0){
+            _.forEach(columns, function(column){
+                var criteria = criterias[column.COLUMN_NAME];
+                if(criteria){
+                    sql += isFirst ? ' WHERE ' : ' AND ';
+                    sql += criteria ? ' ' + column.COLUMN_NAME + '="' + criteria + '"' : '';
+                    isFirst = false;
+                }
+            });
+
+        }
+        sql += ';';
+        $scope.request = sql;
+        $log.info(sql)
+    };
+
+
     $scope.tables = [];
     $scope.getTables = function(){
         $http.post('tables', {con:$scope.con}).success(function(rows){
             $scope.tables = rows;
         });
     };
-    $scope.getTables();
+
     $scope.onTableSelect = function($item, $model, $label){
-        $scope.getColumns($item.TABLE_NAME);
+        $scope.table = $item.TABLE_NAME;
+        $scope.getMetadata($item.TABLE_NAME);
     };
-    $scope.metadata = [];
+    $scope.metadata = {};
     $scope.criterias = {};
-    $scope.getColumns = function(tableName){
+    $scope.getMetadata = function(tableName){
         $http.post('metadata/' + tableName, {con:$scope.con}).success(function(metadata){
             $scope.metadata = metadata;
         });
     };
 
+
+
+    //init
+    $scope.getTables();
 
 }
